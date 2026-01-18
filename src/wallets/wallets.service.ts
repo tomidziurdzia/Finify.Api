7dcee42a3687d14e7c74604df6aedecaf5aef339
TomasLadislaoDziurdz <tomidziurdzia@gmail.com>
2026-01-18 10:41:17 +0100

    add constructor using patron repository

--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wallet } from './entities/wallet.entity';
 
 @Injectable()
 export class WalletsService {
-  create(createWalletDto: CreateWalletDto) {
+  
+  constructor(
+    @InjectRepository(Wallet)
+    private readonly walletRepository : Repository<Wallet>,
+  ) {}
+
+  async create(createWalletDto: CreateWalletDto) {
     return 'This action adds a new wallet';
   }
 
